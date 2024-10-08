c1fb95153b6f81de4bdcff9cd38c3ab7dae0f253
DreamCity <hasanxoliqnazarov087@gmail.com>
2024-10-08 10:32:53 +0500

    Initial commit

---
+++ users.py
@@ -0,0 +1,37 @@
+s= """
+School management system
+1.Login
+2.Register
+->
+"""
+
+users = {
+    "students": {
+        "1": {
+            "fullname": "jack",
+            "username": "jacks",
+            "password": 1111,
+            "grade": 4,
+            "lesson": "Python,Math,English",
+            "class": "10b23",
+            "homeTask": "Decorators"
+        },
+        "2": {
+            "fullname": "Xasan",
+            "username": "xxa571",
+            "password": 2222,
+            "grade": 5,
+            "lesson": "Python,Math,English",
+            "class": "10b23",
+            "homeTask": "Decorators"
+        },
+
+    }
+}
+
+d = """
+1. my profile
+2. my lesson
+3. my class
+4. homeTask
+"""
